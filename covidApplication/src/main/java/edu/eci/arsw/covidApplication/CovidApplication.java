3c9e133ff878684ea32d3a10c8c7697d56983713
julianagarzond <juliana.garzon-d@mail.escuelaing.edu.co>
2020-03-26 07:11:48 -0500

    primer commit

---
+++ covidApplication/src/main/java/edu/eci/arsw/covidApplication/CovidApplication.java
@@ -0,0 +1,13 @@
+package edu.eci.arsw.covidApplication;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CovidApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CovidApplication.class, args);
+	}
+
+}
